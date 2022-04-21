681c15f0ec24f42c20d7d3203a197fb54154a1e1
andrescala13 <andres2214119@correo.uis.edu.co>
2022-04-21 08:05:38 -0500

    Desarrollo: Tipos de datos en java: primitivos vs. referenciados. andrescala13

---
+++ modelo/TipoDatosJava.java
@@ -0,0 +1,28 @@
+package modelo;
+
+public class TipoDatosJava
+
+
+{
+ public static void main(String[] args)
+      
+  
+  {
+      //Tipos de datos primitivos
+      int x = 12;
+      short w = 2;
+      char c = '@';
+      boolean esta = true;
+      System.out.println("Tipos de datos primitivos : ");
+      System.out.println("x = " + x);
+
+      // Tipos de datos referenciados
+      String s = "UIS SOCORRO";
+      int[] a = new int[12];
+      Punto p1 = new Punto(2,3);
+      System.out.println("Tipos de datos referenciados :");
+      System.out.println("Punto: " + p1);
+
+
+  }
+}
